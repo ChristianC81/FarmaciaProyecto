e3082e50a57f4a810a83c81d4106f497365a1cd3
Jhudyt Gaona <jhudyt.gaona.est@tecazuay.edu.ec>
2023-04-26 23:09:19 -0500

    finiquitado xd

--- src/vista/JFPedido.java
+++ src/vista/JFPedido.java
@@ -6,6 +6,7 @@ package vista;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
 
 /**
  *
@@ -36,14 +37,6 @@ public class JFPedido extends javax.swing.JFrame {
         this.btnGuardar = btnGuardar;
     }
 
-    public JLabel getLblPedidoDireccion() {
-        return lblPedidoDireccion;
-    }
-
-    public void setLblPedidoDireccion(JLabel lblPedidoDireccion) {
-        this.lblPedidoDireccion = lblPedidoDireccion;
-    }
-
     public JLabel getLblPedidoMedicamentos() {
         return lblPedidoMedicamentos;
     }
@@ -52,8 +45,7 @@ public class JFPedido extends javax.swing.JFrame {
         this.lblPedidoMedicamentos = lblPedidoMedicamentos;
     }
 
-   
-    
+
     
     
     /**
@@ -69,7 +61,6 @@ public class JFPedido extends javax.swing.JFrame {
         btnGuardar = new javax.swing.JButton();
         btnCancelar = new javax.swing.JButton();
         lblPedidoMedicamentos = new javax.swing.JLabel();
-        lblPedidoDireccion = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -84,34 +75,28 @@ public class JFPedido extends javax.swing.JFrame {
 
         lblPedidoMedicamentos.setForeground(new java.awt.Color(255, 255, 255));
 
-        lblPedidoDireccion.setForeground(new java.awt.Color(255, 255, 255));
-
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(lblPedidoDireccion, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(jPanel2Layout.createSequentialGroup()
-                            .addGap(87, 87, 87)
-                            .addComponent(btnGuardar)
-                            .addGap(58, 58, 58)
-                            .addComponent(btnCancelar))
-                        .addGroup(jPanel2Layout.createSequentialGroup()
-                            .addGap(51, 51, 51)
-                            .addComponent(lblPedidoMedicamentos, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(87, 87, 87)
+                        .addComponent(btnGuardar)
+                        .addGap(58, 58, 58)
+                        .addComponent(btnCancelar))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(51, 51, 51)
+                        .addComponent(lblPedidoMedicamentos, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(52, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                 .addGap(29, 29, 29)
-                .addComponent(lblPedidoMedicamentos, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(lblPedidoMedicamentos, javax.swing.GroupLayout.DEFAULT_SIZE, 118, Short.MAX_VALUE)
                 .addGap(18, 18, 18)
-                .addComponent(lblPedidoDireccion, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnGuardar)
                     .addComponent(btnCancelar))
@@ -141,7 +126,6 @@ public class JFPedido extends javax.swing.JFrame {
     private javax.swing.JButton btnCancelar;
     private javax.swing.JButton btnGuardar;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JLabel lblPedidoDireccion;
     private javax.swing.JLabel lblPedidoMedicamentos;
     // End of variables declaration//GEN-END:variables
 }
